c27c2287f35a28616767d5dd32582784328543e5
MD NIZAM UDDIN <53169290+NizamUddin07@users.noreply.github.com>
2024-09-06 23:32:15 +0600

    calculation added for diposite and total balance

--- Js/deposite.js
+++ Js/deposite.js
@@ -1,14 +1,52 @@
 document.getElementById('btn-deposite').addEventListener('click', function(){
   
     const depositeField = document.getElementById('deposite-field');
-    const depositeAmount = depositeField.value;
+    const newdepositeAmountString = depositeField.value;
+    const newdepositeAmount = parseFloat(newdepositeAmountString);
+    
 
 
     // deposite total 
 
     const depositeTotalElement = document.getElementById('deposite-total');
-    const depositedTotal = depositeTotalElement.innerText;
-    depositeTotalElement.innerText =   depositeAmount;
+    const previousdepositedTotalString = depositeTotalElement.innerText;
+    const previousdepositedTotal = parseFloat(previousdepositedTotalString);
+
+    
+    const currentDepositetotal = previousdepositedTotal + newdepositeAmount;
+
+    depositeTotalElement.innerText= currentDepositetotal;
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    // balance total 
+
+    const balancetoalElement= document.getElementById('total-balance');
+    const previousBalanceTotalString = balancetoalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+
+
+    // calculate current total
+    const currentTotalBalance = previousBalanceTotal + newdepositeAmount;
+    
+
+    // set the value to show on website 
+    balancetoalElement.innerText = currentTotalBalance;
+
+    // delete value 
+    depositeField.value = '';
 
    
 })
