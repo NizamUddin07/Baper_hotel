9b016f9cc819cb4fe056fc85bbef2e864960f891
MD NIZAM UDDIN <53169290+NizamUddin07@users.noreply.github.com>
2024-09-06 11:47:28 +0600

    validation for email and password

---
+++ Js/login.js
@@ -0,0 +1,24 @@
+
+// Step Number One
+document.getElementById('btn-submit').addEventListener('click', function(){
+    
+    // Step 2, Get the Email id 
+    const emailField=document.getElementById('user-email');
+    const email=emailField.value;
+    
+    // Step 3, Get password 
+    const passwordField=document.getElementById('user-password');
+    const password = passwordField.value;
+   
+    // validation of email and password
+
+
+    if(email=='baperhotel@gmail.com' && password =='password'){
+        console.log('valid user')
+    }
+    else{
+        console.log('invalid user');
+    }
+
+})
+
